32b86fbf7edcc0c05033727a3139c11c2ab06e74
innjie <innjie@naver.com>
2022-09-21 01:08:41 +0900

    [add] study-python

---
+++ Practice01.py
@@ -0,0 +1,30 @@
+# 정수 / 실수 자료형
+print(5)  # 정수
+print(-10)
+print(3.14)  # 실수
+print(1000)
+print(5 + 3)
+print(2 * 8)
+print(3 * (3 + 1))
+
+# 문자열 자료형
+print('풍선')
+print("나비")
+print("a" * 9)
+
+# boolean 자료형
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))  # True
+
+# operation
+print(2 ** 3)  # 8
+print(5 % 2)  # 3
+print(5 // 3)  # 1 (몫)
+print(10 < 3)
+print(5 <= 5)
+print((3 > 0) and (3 < 5))  # True
---
+++ Practice02.py
@@ -0,0 +1,19 @@
+# math method
+print(abs(-5))  # 5
+print(pow(4, 2)) # 4의 2제곱
+print(max(5, 12)) # 12
+print(round(3.14))  # 3
+print(round(4.59))  # 5
+
+from math import *
+print(floor(4.99))  # 4
+print(ceil(3.14))  # 4
+print(sqrt(16))  # 4
+
+from random import *
+print(random())  # 난수 생성
+print(randrange(1, 46)) # 1 ~ 46 미만 난수 생성
+print(randint(1, 45)) # 1 ~ 45 포함 난수 생성
+
+# Quiz
+print(randint(4, 28))
