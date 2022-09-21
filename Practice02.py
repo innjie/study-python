b30dff3141fe426f5bead85a148a9e07e3a75dab
innjie <innjie@naver.com>
2022-09-21 22:57:24 +0900

    [study] python lecture

--- Practice02.py
+++ Practice02.py
@@ -17,3 +17,41 @@ print(randint(1, 45)) # 1 ~ 45 포함 난수 생성
 
 # Quiz
 print(randint(4, 28))
+
+# slicing
+userNo = '990120-1234567'
+
+python = 'python is amazing'
+print(python.lower())
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "java"))
+
+index = python.index("n")
+print(index)
+index = python.index("n", index + 1)
+
+print(python.find("n"))
+print(python.find('java'))  # -1
+#print(python.index("Java"))  # error
+print(python.count("n"))
+
+# 01.
+print("나는 %d살 입니다." % 20)
+print('나는 %s를 좋아합니다.' % '파이썬')
+print('Apple은 %c로 시작합니다.' % 'a')
+print('나는 %s색과 %s색을 좋아합니다.' %('파란', '빨간'))
+
+# 02.
+print('나는 {}살입니다.' .format(20))
+print('나는 {}색과 {}색을 좋아해요.' .format('파란', '빨간'))
+print('나는 {1}색과 {0}색을 좋아해요.' .format('파란', '빨간'))  # 반대로 출력
+
+# 03.
+print('나는 {age}살이며, {color}색을 좋아해요.' .format(age = 20, color = '빨간'))
+
+# 04.
+age = 20
+color = '빨간'
+print(f'나는 {age}살이며, {color}색을 좋아해요.')
